91dccb7980d4a970af1e553d26ef0a6227380b06
Chevy Lin <chevylin0802@gmail.com>
2020-02-25 15:51:44 +0800

    update for QRScanner

--- app/src/main/java/com/example/smarttalk/MainActivity.java
+++ app/src/main/java/com/example/smarttalk/MainActivity.java
@@ -19,6 +19,8 @@ import android.widget.Toast;
 
 import com.example.smarttalk.packages.PInfo;
 import com.example.smarttalk.packages.Packages;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.util.ArrayList;
 
@@ -94,6 +96,9 @@ public class MainActivity extends AppCompatActivity {
 
         mPackages = new Packages(this);
         mApps = mPackages.getPackages();
+
+        initScanner();
+
         return;
     }
 
@@ -102,6 +107,32 @@ public class MainActivity extends AppCompatActivity {
         mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
         mSpeechRecognizer.setRecognitionListener(new listener());
         super.onStart();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void initScanner() {
+        new IntentIntegrator(this)
+                // 自定义Activity，重点是这行----------------------------
+                .setCaptureActivity(CustomCaptureActivity.class)
+                .setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)// 扫码的类型,可选：一维码，二维码，一/二维码
+                .setPrompt("请对准二维码")// 设置提示语
+                .setCameraId(0)// 选择摄像头,可使用前置或者后置
+                .setBeepEnabled(true)// 是否开启声音,扫完码之后会"哔"的一声
+                .setBarcodeImageEnabled(true)// 扫完码之后生成二维码的图片
+                .initiateScan();// 初始化扫码
     }
 
     private void sendMessage(String msg) {
